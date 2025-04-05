cda6a69070d1c370acd0c7b1e6c9a1f483d712c0
Haziel <business@ghostix.network>
2025-04-04 21:47:25 -0300

    Defined initial file structure

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
 import './style.css'
+import Phaser from 'phaser';
 
+const config = {
+    title: "Hazu's Portfolio",
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360,
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        "arcade": {
+            gravity: {
+                y: 2000
+            }
+        }
+    },
+    scene: [
+        Preloader,
+        UI,
+        Play,
+        Menu
+    ]
+};
+
+new Phaser.Game(config);
